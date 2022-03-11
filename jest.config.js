c39c10e19133712983fd82fabbf6cf077bfc5a92
Toji Takuro <takuro.toji@gmail.com>
2022-03-12 01:41:06 +0900

    スナップショットのテスト環境を仮作成
    - npm i -D @storybook/addon-storyshots
    
    以下を参考にしたがテスト実行に失敗する
    
    https://storybook.js.org/tutorials/intro-to-storybook/angular/ja/simple-component/

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,24 @@ module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
   globalSetup: "jest-preset-angular/global-setup",
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.html$",
+      diagnostics: false,
+      isolatedModules: true,
+    },
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "<rootDir>/src/jest-config/__mocks__/styleMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/jest-config/__mocks__fileMock.js",
+  },
+  transform: {
+    "^.+\\.(ts|html)$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.stories\\.[jt]sx?$": "@storybook/addon-storyshots/injectFileName",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", ".html"],
+  transformIgnorePatterns: ["node_modules/(?!(@storybook/addon-storyshots)/)"],
 };
