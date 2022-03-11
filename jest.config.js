a1326f5e33ac4b48665f3383f1c035bd4d854bc7
Toji Takuro <takuro.toji@gmail.com>
2022-03-12 00:24:42 +0900

    jest-preset-angular の README を参考に設定を変更
    以下のログが出るので対応。
    `ng-jest-processor is deprecated and will be removed in the next major version v12. Please use jest-preset-angular/global-setup in Jest config instead`
    
    https://github.com/thymikee/jest-preset-angular/blob/main/README.md

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ require("jest-preset-angular/ngcc-jest-processor");
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
+  globalSetup: "jest-preset-angular/global-setup",
 };
